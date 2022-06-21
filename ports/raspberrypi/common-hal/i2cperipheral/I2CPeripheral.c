c66b061978797176b0f28e128fe6baedee1b1948
Ken Stillson <ken@point0.net>
2022-06-20 23:54:24 -0400

    try a very simplistic implementation for getting and sending bytes.
    The next-level down code I'm calling (rp2_common/hardware_i2c/i2c.c)
    doesn't seem to say much about ack'ing.  I wonder if it's built-in
    to the next level code and we don't need to worry about it.. dunno.
    
    this doesn't work yet, btw.  at the moment, the test client sees the
    0x42 address during it's scan, but both read and write attempts
    time-out.  Not sure what the problem is yet.

--- ports/raspberrypi/common-hal/i2cperipheral/I2CPeripheral.c
+++ ports/raspberrypi/common-hal/i2cperipheral/I2CPeripheral.c
@@ -24,6 +24,11 @@
  * THE SOFTWARE.
  */
 
+/* TEMP kds notes:
+   next level down I2C functionality appears to come from:
+   ports/raspberrypi/sdk/src/rp2_common/hardware_i2c/i2c.c
+ */
+
 #include "shared-bindings/i2cperipheral/I2CPeripheral.h"
 
 #include "common-hal/busio/I2C.h"
@@ -48,10 +53,10 @@ STATIC i2c_inst_t *i2c[2] = {i2c0, i2c1};
 
 void common_hal_i2cperipheral_i2c_peripheral_construct(
     i2cperipheral_i2c_peripheral_obj_t *self,
-    const mcu_pin_obj_t *scl, 
+    const mcu_pin_obj_t *scl,
     const mcu_pin_obj_t *sda,
-    uint8_t *addresses, 
-    unsigned int num_addresses, 
+    uint8_t *addresses,
+    unsigned int num_addresses,
     bool smbus
 ){
 
@@ -60,9 +65,6 @@ void common_hal_i2cperipheral_i2c_peripheral_construct(
     mp_printf(&mp_plat_print, "i2cperipheral - num_addresses: %d.\n", num_addresses);
     mp_printf(&mp_plat_print, "i2cperipheral - sda: %p, scl: %p.\n", sda->number,scl->number);
 
-    // DEBUG: static set address.
-    //mp_int_t address = 0x12;
-
     // I2C pins have a regular pattern. SCL is always odd and SDA is even. They match up in pairs
     // so we can divide by two to get the instance. This pattern repeats.
     size_t scl_instance = (scl->number / 2) % 2;
@@ -70,13 +72,15 @@ void common_hal_i2cperipheral_i2c_peripheral_construct(
     if (scl->number % 2 == 1 && sda->number % 2 == 0 && scl_instance == sda_instance) {
         self->peripheral = i2c[sda_instance];
     }
-    
+
     if (self->peripheral == NULL) {
         mp_raise_ValueError(translate("Invalid pins"));
     }
     if ((i2c_get_hw(self->peripheral)->enable & I2C_IC_ENABLE_ENABLE_BITS) != 0) {
         mp_raise_ValueError(translate("I2C peripheral in use"));
     }
+
+    self->addresses = addresses;  // @@kds
 
     const uint32_t frequency = 400000;
     i2c_init(self->peripheral, frequency);
@@ -94,14 +98,14 @@ void common_hal_i2cperipheral_i2c_peripheral_construct(
 
 
 bool common_hal_i2cperipheral_i2c_peripheral_deinited(i2cperipheral_i2c_peripheral_obj_t *self) {
-    
+
     mp_printf(&mp_plat_print, "i2cperipheral - you're here: deinited [%b]\n", self->sda_pin);
     return self->sda_pin == NO_PIN;
 
 }
 
 void common_hal_i2cperipheral_i2c_peripheral_deinit(i2cperipheral_i2c_peripheral_obj_t *self) {
-    
+
     if (common_hal_i2cperipheral_i2c_peripheral_deinited(self)) {
         return;
     }
@@ -109,27 +113,26 @@ void common_hal_i2cperipheral_i2c_peripheral_deinit(i2cperipheral_i2c_peripheral
     i2c_deinit(self->peripheral);
     mp_printf(&mp_plat_print, "i2cperipheral - you're here: deinit.\n");
     return;
-   
+
 }
 
 static int i2c_peripheral_check_error(i2cperipheral_i2c_peripheral_obj_t *self, bool raise) {
-   
+
     mp_printf(&mp_plat_print, "i2cperipheral - you're here: checking for error. [%b]\n",raise);
-   
+
     return 0;
 }
 
 int common_hal_i2cperipheral_i2c_peripheral_is_addressed(
-    i2cperipheral_i2c_peripheral_obj_t *self, 
-    uint8_t *address, 
-    bool *is_read, 
+    i2cperipheral_i2c_peripheral_obj_t *self,
+    uint8_t *address,
+    bool *is_read,
     bool *is_restart
 ) {
-    
-    mp_printf(&mp_plat_print, "i2cperipheral - you're here: is addressed [num address: %d].\n",self->num_addresses);
-    
-    int err = i2c_peripheral_check_error(self, false);
 
+    mp_printf(&mp_plat_print, "i2cperipheral - you're here: is addressed [num address: %d].\n",self->num_addresses);
+
+    int err = i2c_peripheral_check_error(self, false);
     if (err) {
         mp_printf(&mp_plat_print, "i2cperipheral - you're here: error in is_addressed.\n");
         return err;
@@ -137,7 +140,8 @@ int common_hal_i2cperipheral_i2c_peripheral_is_addressed(
 
     self->writing = false;
 
-    *address = 0x41; //self->addresses[0];
+    //kds *address = 0x41; //self->addresses[0];
+
     *is_read = ((self->peripheral->hw->raw_intr_stat & I2C_IC_RAW_INTR_STAT_RD_REQ_BITS) !=0);
     *is_restart = false; //???
 
@@ -157,20 +161,29 @@ int common_hal_i2cperipheral_i2c_peripheral_is_addressed(
 }
 
 int common_hal_i2cperipheral_i2c_peripheral_read_byte(
-    i2cperipheral_i2c_peripheral_obj_t *self, 
+    i2cperipheral_i2c_peripheral_obj_t *self,
     uint8_t *data
 ) {
-    
-    mp_printf(&mp_plat_print, "i2cperipheral - you're here: read byte\n");
-    return 1;
 
+    mp_printf(&mp_plat_print, "i2cperipheral - you're here: read byte\n");
+
+    uint8_t buffer;
+    int rslt = i2c_read_blocking(self->peripheral, self->addresses[0], &buffer, 1, true);
+    mp_printf(&mp_plat_print, "i2c_read_blocking returned %d", rslt);
+
+    // That returned byte-count, so anything but 1 is bad.
+    return (rslt == 1) ? 0 : 1;
 }
 
 int common_hal_i2cperipheral_i2c_peripheral_write_byte(i2cperipheral_i2c_peripheral_obj_t *self, uint8_t data) {
-    
+
     mp_printf(&mp_plat_print, "i2cperipheral - you're here: write byte\n");
 
-    return 1;
+    int rslt = i2c_write_blocking(self->peripheral, self->addresses[0], &data, 1, true);
+    mp_printf(&mp_plat_print, "i2c_write_blocking returned %d", rslt);
+
+    // That returned byte-count, so anything but 1 is bad.
+    return (rslt == 1) ? 0 : 1;
 }
 
 void common_hal_i2cperipheral_i2c_peripheral_ack(i2cperipheral_i2c_peripheral_obj_t *self, bool ack) {
@@ -181,5 +194,4 @@ void common_hal_i2cperipheral_i2c_peripheral_ack(i2cperipheral_i2c_peripheral_ob
 void common_hal_i2cperipheral_i2c_peripheral_close(i2cperipheral_i2c_peripheral_obj_t *self) {
  mp_printf(&mp_plat_print, "i2cperipheral - you're here: peripheral close.\n");
   return;
-
 }
